214a46c8b75dd49af1df0c81636a0772c7a18d92
Kevin Hwangpo <hwangpo3@gmail.com>
2018-07-17 17:42:20 -0700

    getting started, nothing finished, bunch of errors

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render as renderDom } from 'react-dom';
 import App from './components/app/app';
 
-class Main extends React.component {
+class Main extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <App /> 
+       <App />
         <h1>Did we make it?</h1>
       </React.Fragment>
     );
